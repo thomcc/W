a977a0095cc113226c5bae531280eca96d666d0a
thom <chiovolonit@gmail.com>
2011-12-22 21:42:52 -0500

    start of level rewrite

---
+++ source/levelgraph.rkt
@@ -0,0 +1,37 @@
+#lang racket
+(require racket/class 
+         racket/set 
+         racket/list 
+         "utils.rkt"
+         "tile.rkt")
+
+
+     
+  
+(define level%
+  (class object% (super-new)
+    (define/public (get-rooms) (void)) 
+    (define/public (get-paths) (void))
+    
+    (define/public (get-outgoing room) (void))
+    (define/public (get-incoming room) (void))
+    
+    (define/public (get-end-rooms path) (void))
+    
+    (define/public (add-room room) (void))
+    (define/public (add-path room room type) (void))
+    
+    (define/public (remove-room room) (void))
+    (define/public (remove-path path) (void))
+    
+    (define/public (connected? room room) 
+      (void))
+      
+      
+      
+      
+      
+      
+      
+      
+
