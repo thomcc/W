67525b16157125106284ad0fada988ff1c855f74
thom <chiovolonit@gmail.com>
2011-12-26 00:05:44 -0500

    structure type properties

--- source/deftile.rkt
+++ source/deftile.rkt
@@ -1,4 +1,11 @@
 #lang racket
+(define-values (prop:render-name has-render-name? render-name)
+  (make-struct-type-property 'render-name))
+(define-values (prop:solid? has-solid? solid?)
+  (make-struct-type-property 'solid?))
+(define-values (prop:deadly? has-deadly? deadly?)
+  (make-struct-type-property 'deadly?))
+
 (struct tile (room posn [on-use #:auto] [on-step #:auto])
   #:transparent #:mutable #:auto-value '()
   #:property prop:render-name 'tile
